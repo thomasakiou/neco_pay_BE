8678a75f8e7e1098433bd6ce37e1a9c3354d5ae4
Thomas <thomas.akiou@gmail.com>
2025-12-10 12:01:00 +0100

    new

--- start_server.bat
+++ start_server.bat
@@ -1,3 +1,30 @@
+@REM @echo off
+@REM cd /d "D:\Projects\Payment-App\neco-payment-manager-BE"
+
+@REM call .venv\Scripts\activate.bat
+@REM uvicorn app.main:app --reload
+
+
+@REM @echo off
+@REM REM Navigate to project directory
+@REM cd /d "D:\Projects\Payment-App\neco-payment-manager-BE"
+
+@REM REM Activate virtual environment
+@REM call .venv\Scripts\activate.bat
+
+@REM REM Start the server (no --reload)
+@REM uvicorn app.main:app --host 0.0.0.0 --port 8000
+
+@REM REM Optional: keep window open to see errors
+@REM pause
+
+
+
 @echo off
-cd /d G:\Projects\neco-payment-manager-BE
-uvicorn app.main:app --reload
+cd /d "D:\Projects\Payment-App\neco-payment-manager-BE"
+
+REM Activate virtual environment
+call .venv\Scripts\activate.bat
+
+REM Start backend
+start "" uvicorn app.main:app --host 0.0.0.0 --port 8000
